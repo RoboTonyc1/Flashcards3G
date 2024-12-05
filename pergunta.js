273fcb3dd3ec2c715433cd203199ecc2bf000da1
RoboTonyc1 <antoniopepinelli@professor.educacao.sp.gov.br>
2024-12-05 15:48:39 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Sistema Digestório',
+    'Em quais órgãos são digeridas as proteínas?',
+    'No estômago e no duodeno (intestino delgado).'
+)
+criaCartao(
+    'Endocrinologia',
+    'Qual o hormônio com efeito oposto à insulina?',
+    'É o Glucagon.'
+)
+
+criaCartao(
+    'Endocrinologia',
+    'Qual doença relacionada a quadros de hiperglicemia e ausência de insulina?',
+    'Diabetes.'
+
+)
+
+criaCartao(
+    'Endocrinologia',
+    'Quais hormônios produzidos pelo pâncreas?',
+    'Insulina e Glucagon'
+)
